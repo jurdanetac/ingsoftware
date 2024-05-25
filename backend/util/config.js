dc3eff8e6bf68f7ccbb419306d8d3e6e99986990
Juan Urdaneta <jurdanetac10@gmail.com>
2024-05-25 12:27:14 -0400

    Add env file for local use in showcase

--- backend/util/config.js
+++ backend/util/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
   DEBUG: process.env.DEBUG,
   NODE_VERSION: process.env.NODE_VERSION,

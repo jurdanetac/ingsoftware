c1e6ebe971adb279e1a408f9dc16ec2115f9c192
Juan Urdaneta <jurdanetac10@gmail.com>
2024-05-26 12:48:05 -0400

    Add migration rollback script

--- backend/util/db.js
+++ backend/util/db.js
@@ -25,17 +25,19 @@ if (NODE_ENV === "production") {
 
 const sequelize = new Sequelize(DATABASE_URL, options);
 
-const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: "migrations/*.js",
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  });
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
 
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
   const migrations = await migrator.up();
+
   console.log("Migrations up to date", {
     files: migrations.map((mig) => mig.name),
   });
@@ -67,4 +69,10 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { connectToDatabase, sequelize };
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+
+module.exports = { connectToDatabase, sequelize, rollbackMigration }
